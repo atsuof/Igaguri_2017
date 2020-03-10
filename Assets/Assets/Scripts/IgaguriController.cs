ea1665d341c8f64116b9e44733f518422acc5f52
Fukuda Atsuo <a.tuuus.kod.f@gmail.com>
2020-03-10 13:59:09 +0900

    firast commit

---
+++ Assets/Assets/Scripts/IgaguriController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour {
+
+    public void shoot(Vector3 dir)
+    {
+		GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+	// Use this for initialization
+	void Start () {
+		//shoot(new Vector3(0, 200, 2000));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+		if (collision.gameObject.name.Equals("target"))
+		{
+			GetComponent<Rigidbody>().isKinematic = true;
+		}
+		GetComponent<ParticleSystem>().Play();
+
+    }
+}
